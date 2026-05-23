e4e77bd3fe0d53f3b6d47d4e1b5ca7fd49510a04
Douglas P. Fields, Jr. <symbolics@lisp.engineer>
2026-05-23 18:05:01 -0400

    Print the docstring on invoke-generic

--- main.lisp
+++ main.lisp
@@ -22,13 +22,15 @@
 ;; Expose our C# function "add3" by creating the package and exported
 ;; symbol during compilation.
 
-
 ;; Test Add3
 (format t "Add3 sum = ~A~%" (monoutils:add3 1 2.0 9/3))
 (handler-case
     (monoutils:add3 1 "two" 3)
   (type-error (c)
     (format t "Caught expected type error: ~A~%" c)))
+
+(format *error-output* "invoke-generic docs: ~A~%"
+  (documentation 'monoutils:invoke-generic 'function))
 
 ;; Test call-base functionality
 (let ((child (dotnet:new "Child")))
