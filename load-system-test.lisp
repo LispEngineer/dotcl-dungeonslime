d6b3dea40c2f7fccbe6ba5d0029d0ad9485275d4
Douglas P. Fields, Jr. <symbolics@lisp.engineer>
2026-05-30 13:18:11 -0400

    Make the test work at REPL and at build/run time.

--- load-system-test.lisp
+++ load-system-test.lisp
@@ -15,17 +15,24 @@
 
 ;; To get this effort to work at compile time, need to include this in an
 ;; eval-when. It forces the system to compile the anaphora package and
-;; load it during compilation. BUT, this now breaks the regular (load "...")!
+;; load it during compilation.
+;;
+;; If I put both these forms (require & load-system) into the same `eval-when`,
+;; then it breaks during (load "...") of this file at the REPL. For some
+;; reason, separating them makes it work correctly.
 ;;
 ;; NOTE: It does NOT put the FASL into the final build, so that has to
 ;; be done separately somehow. So you can't copy the game directory
 ;; to another computer/user and expect it to work, yet.
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (require "asdf")
+  (require "asdf"))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
   ;; If needed, you can push search paths to asdf:*central-registry* here
   (asdf:load-system "anaphora"))
 
-(format *error-output* "[load-system-test.lisp] central-registry = ~A~%" asdf:*central-registry*)
+(format *error-output* "[load-system-test.lisp] (length asdf:*central-registry*) = ~A~%" 
+        (length asdf:*central-registry*))
 
 (anaphora:awhen (+ 3 4)
-  (format *error-output* "[load-system-test.lisp] anaphora:it = ~A~%" anaphora:it))
+  (format *error-output* "[load-system-test.lisp] anaphora:it = ~A (should be 7)~%" anaphora:it))
